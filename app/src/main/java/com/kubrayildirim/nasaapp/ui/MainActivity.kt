3b6639d86aa5a07462f44a2c8baeccc2381281a1
upskubra <yildirimk268@gmail.com>
2022-11-09 00:25:08 +0300

    add di requirements

--- app/src/main/java/com/kubrayildirim/nasaapp/ui/MainActivity.kt
+++ app/src/main/java/com/kubrayildirim/nasaapp/ui/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.kubrayildirim.nasaapp.R
 import com.kubrayildirim.nasaapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
