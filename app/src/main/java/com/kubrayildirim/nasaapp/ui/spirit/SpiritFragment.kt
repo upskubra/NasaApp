399b613b99f6f6b769b4ffdc9d6b4c2b79f91b2f
upskubra <yildirimk268@gmail.com>
2022-11-09 03:57:32 +0300

    set spirit screen

---
+++ app/src/main/java/com/kubrayildirim/nasaapp/ui/spirit/SpiritFragment.kt
@@ -0,0 +1,54 @@
+package com.kubrayildirim.nasaapp.ui.spirit
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.kubrayildirim.nasaapp.adapter.BaseAdapter
+import com.kubrayildirim.nasaapp.data.model.Photo
+import com.kubrayildirim.nasaapp.databinding.FragmentSpiritBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class SpiritFragment : Fragment() {
+    private lateinit var binding: FragmentSpiritBinding
+    private val spiritViewModel: SpiritViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSpiritBinding.inflate(inflater)
+        updateSpiritState()
+        return binding.root
+    }
+
+    private fun updateSpiritState() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            spiritViewModel.getSpirit()
+            spiritViewModel.spiritState.collect {
+                if (it.isLoading) {
+                    binding.progressSpirit.visibility = View.VISIBLE
+                    binding.rvSpirit.visibility = View.GONE
+                } else if (it.photoList?.photos?.isNotEmpty() == true) {
+                    binding.progressSpirit.visibility = View.GONE
+                    initRecycler(it.photoList.photos)
+                } else {
+                    Toast.makeText(context, "No Data Found", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun initRecycler(list: List<Photo>) {
+        binding.rvSpirit.apply {
+            adapter = BaseAdapter(list)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/kubrayildirim/nasaapp/ui/spirit/SpiritViewModel.kt
@@ -0,0 +1,51 @@
+package com.kubrayildirim.nasaapp.ui.spirit
+
+import android.app.Application
+import com.kubrayildirim.nasaapp.data.model.RoverViewState
+import com.kubrayildirim.nasaapp.data.remote.NetworkResult
+import com.kubrayildirim.nasaapp.data.repository.RoverRepository
+import com.kubrayildirim.nasaapp.ui.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class SpiritViewModel @Inject constructor(
+    private val roverRepository: RoverRepository,
+    application: Application
+) : BaseViewModel(application) {
+    private val _spiritState = MutableStateFlow(RoverViewState())
+    val spiritState: StateFlow<RoverViewState> = _spiritState.asStateFlow()
+
+    suspend fun getSpirit() {
+        roverRepository.fetchSpirit().collect { result ->
+            when (result) {
+                is NetworkResult.Success -> {
+                    _spiritState.value =
+                        result.data?.let {
+                            RoverViewState(
+                                photoList = it,
+                                isLoading = false
+                            )
+                        }!!
+                }
+                is NetworkResult.Error -> {
+                    _spiritState.update {
+                        it.copy(
+                            isLoading = false,
+                            error = result.message ?: "An unexpected error occurred"
+                        )
+                    }
+                }
+                is NetworkResult.Loading -> {
+                    _spiritState.update {
+                        it.copy(isLoading = true)
+                    }
+                }
+            }
+        }
+    }
+}
