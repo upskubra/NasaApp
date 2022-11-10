962843b0a6b30e254fcb73830dadc69d9c982935
upskubra <yildirimk268@gmail.com>
2022-11-10 21:22:05 +0300

    add new view state

--- app/src/main/java/com/kubrayildirim/nasaapp/data/model/RoverViewState.kt
+++ app/src/main/java/com/kubrayildirim/nasaapp/data/model/RoverViewState.kt
@@ -3,6 +3,7 @@ package com.kubrayildirim.nasaapp.data.model
 data class RoverViewState(
     val isLoading: Boolean = true,
     val photoList: RoverModel? = null,
-    val error: String = ""
+    val error: String = "",
+    val cameraList: HashSet<String>? =null
 )
 
