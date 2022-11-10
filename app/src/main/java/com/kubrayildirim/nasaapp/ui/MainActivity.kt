7f9b96cd517659e484da9a22cb5f6bd605953f9a
upskubra <yildirimk268@gmail.com>
2022-11-10 21:19:02 +0300

    add toolbar

--- app/src/main/java/com/kubrayildirim/nasaapp/ui/MainActivity.kt
+++ app/src/main/java/com/kubrayildirim/nasaapp/ui/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        setSupportActionBar(binding.toolbar)
         setNavigationBar()
     }
 
